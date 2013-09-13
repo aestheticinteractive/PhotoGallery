e56caede1ee975eb794fee53e6b74a9e5385b1f6
Zach Kinstner <zach@aestheticinteractive.com>
2013-09-13 19:33:07 -0400

    Removed unused server-side EXIF code.

--- Solution/PhotoGallery.Services/Util/ImageUtil.cs
+++ Solution/PhotoGallery.Services/Util/ImageUtil.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Text;
 
 namespace PhotoGallery.Services.Util {
 
@@ -39,53 +35,6 @@ namespace PhotoGallery.Services.Util {
 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
-		/*--------------------------------------------------------------------------------------------*/
-		//adapted from: www.switchonthecode.com/tutorials/getting-image-metadata-with-csharp
-		public static Dictionary<PropertyTagId, KeyValuePair<PropertyTagType, Object>>
-																		BuildPropMap(Image pMetaImage) {
-			Dictionary<PropertyTagId, KeyValuePair<PropertyTagType, Object>> propMap =
-			new Dictionary<PropertyTagId, KeyValuePair<PropertyTagType, object>>();
-
-			foreach ( PropertyItem p in pMetaImage.PropertyItems ) {
-				Object propValue = new Object();
-
-				switch ( (PropertyTagType)p.Type ) {
-					case PropertyTagType.ASCII:
-						ASCIIEncoding encoding = new ASCIIEncoding();
-						propValue = encoding.GetString(p.Value, 0, p.Len-1);
-						break;
-
-					case PropertyTagType.Int16:
-						propValue = BitConverter.ToInt16(p.Value, 0);
-						break;
-
-					case PropertyTagType.SLONG:
-					case PropertyTagType.Int32:
-						propValue = BitConverter.ToInt32(p.Value, 0);
-						break;
-
-					case PropertyTagType.SRational:
-					case PropertyTagType.Rational:
-						UInt32 numer = BitConverter.ToUInt32(p.Value, 0);
-						UInt32 denom = BitConverter.ToUInt32(p.Value, 4);
-						if ( denom != 0 ) { propValue = (numer/(double)denom).ToString(); }
-						else { propValue = "0"; }
-						if ( propValue.ToString() == "NaN" ) { propValue = "0"; }
-						break;
-
-					case PropertyTagType.Undefined:
-						propValue = "Undefined Data";
-						break;
-				}
-
-				propMap.Add(NumToEnum<PropertyTagId>(p.Id),
-					new KeyValuePair<PropertyTagType, object>(
-						NumToEnum<PropertyTagType>(p.Type), propValue));
-			}
-
-			return propMap;
-		}
-
 		/*--------------------------------------------------------------------------------------------*/
 		public static T NumToEnum<T>(int pNumber) {
 			return (T)Enum.ToObject(typeof(T), pNumber);
