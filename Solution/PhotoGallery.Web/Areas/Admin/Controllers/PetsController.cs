330e20d971eafe3df3e08d68db08074305af11eb
Zach Kinstner <zach@aestheticinteractive.com>
2014-05-15 13:27:02 -0400

    Updated the Admin Pets controller.

--- Solution/PhotoGallery.Web/Areas/Admin/Controllers/PetsController.cs
+++ Solution/PhotoGallery.Web/Areas/Admin/Controllers/PetsController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using PhotoGallery.Services.Account.Tools;
 using PhotoGallery.Services.Admin;
 using PhotoGallery.Web.Application;
+using PhotoGallery.Web.Areas.Admin.Models;
 using PhotoGallery.Web.Controllers;
 
 namespace PhotoGallery.Web.Areas.Admin.Controllers {
@@ -33,6 +34,8 @@ namespace PhotoGallery.Web.Areas.Admin.Controllers {
 			vPets.AddDog("Gulliver Kinstner", "Lhasa Apso, pet", null,
 				(long)LiveArtifactId.LhasaApso, gulliverOwners);*/
 
+
+
 			/*IList<long> chewyOwners = new List<long>();
 			chewyOwners.Add(58694628326506496); //dave
 			chewyOwners.Add(58694638811217920); //pat
@@ -40,29 +43,45 @@ namespace PhotoGallery.Web.Areas.Admin.Controllers {
 			vPets.AddDog("Chewbacca McDonald", "Dog, pet", null,
 				(long)LiveArtifactId.Dog, chewyOwners);*/
 
+
+
 			/*IList<long> boomerOwners = new List<long>();
 			boomerOwners.Add(58694649307463680); //david
 			boomerOwners.Add(58694670308343808); //jen
 			boomerOwners.Add(58694680783618048); //caleb
 
 			vPets.AddDog("Boomer McDonald", "Dog, pet", null,
-				(long)LiveArtifactId.Dog, boomerOwners);
+				(long)LiveArtifactId.Dog, boomerOwners);*/
 
-			IList<long> pixieOwners = new List<long>();
+
+
+			/*IList<long> pixieOwners = new List<long>();
 			pixieOwners.Add(58693020653977600); //rick
 			pixieOwners.Add(58693020958064640); //sue
 
 			vPets.AddDog("Pixie Kinstner", "Dog, pet", null,
-				(long)LiveArtifactId.Dog, pixieOwners);
+				(long)LiveArtifactId.Dog, pixieOwners);*/
 
-			IList<long> kateOwners = new List<long>();
+
+
+			/*IList<long> kateOwners = new List<long>();
 			kateOwners.Add(58694360862031872); //lance
 			kateOwners.Add(58694523385020416); //julia
 
 			vPets.AddDog("Kate Kinstner", "Great Dane, pet", null,
 				(long)LiveArtifactId.GreatDane, kateOwners);*/
 
-			return View();
+			
+			
+			/*IList<long> anyaOwners = new List<long>();
+			anyaOwners.Add(60256745309601792); //joanne
+
+			vPets.AddDog("Anya Khanani", "Dog, pet", null,
+				(long)LiveArtifactId.Dog, anyaOwners);*/
+
+
+
+			return View(new HomeModel());
 		}
 
 	}
